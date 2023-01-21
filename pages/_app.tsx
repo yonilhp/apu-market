8754bf45b05c0cc5d25e78dad32a11d4e9b76733
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2023-01-21 10:03:38 -0500

    termine la parte del lista de productos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { lightTheme } from '@/themes';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+ function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx:DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps 
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
