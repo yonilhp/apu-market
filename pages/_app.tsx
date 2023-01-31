a649fdf3998d4c6b07fe9e0566db39a22ee3c7db
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2023-01-31 10:58:37 -0500

    termine la parte6 del cap14

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,20 @@ import { lightTheme } from '@/themes';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr';
 
  function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={ lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,24 @@ import { Typography } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
 
 import { ProductList } from '@/components/products';
+import { useProducts } from '@/hooks';
 
-import useSWR from "swr"
-const fetcher = (...args:[key:string]) => fetch(...args).then(res => res.json());
+
 
 const HomePage: NextPage = () => {
-  const { data, error, isLoading } = useSWR("/api/products", fetcher);
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-
-  console.log({data});
+  const {products, isLoading} = useProducts('/products');
   return (
     <ShopLayout title={'Apu Market - Home'} pageDescription={'Encuentra los mejores productos de Apu Market aquí'}>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-    <ProductList products={data}/>
+        {
+          isLoading
+          ? <h1>Cargando..</h1>
+          : <ProductList products={products}/>
+        }
+
+    
 
     </ShopLayout>
   )
