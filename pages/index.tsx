e5ecfc93455f750137c6a7f6a5c828ecf7255e01
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2023-01-31 11:23:07 -0500

    termine la parte7 del cap14

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { ShopLayout } from '../components/layouts';
 
 import { ProductList } from '@/components/products';
 import { useProducts } from '@/hooks';
+import { FullScreenLoading } from '@/components/ui';
 
 
 
@@ -17,7 +18,7 @@ const HomePage: NextPage = () => {
 
         {
           isLoading
-          ? <h1>Cargando..</h1>
+          ? <FullScreenLoading/>
           : <ProductList products={products}/>
         }
 
