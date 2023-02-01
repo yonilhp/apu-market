efefa86d0187d4394e674fccbff25d6b0620ee51
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2023-02-01 09:32:18 -0500

    termine la parte8 del cap14

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -11,6 +11,7 @@ interface Props {
 export const ProductCard: FC<Props> = ({ product }) => {
 
     const [isHovered, setIsHovered] = useState(false);
+    const [isImageLoaded, setisImageLoaded] = useState(false);
 
     const productImage = useMemo(() => {
         return isHovered
@@ -35,6 +36,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
                             className='fadeIn'
                             image={ productImage }
                             alt={ product.title }
+                            onLoad={ () => setisImageLoaded(true) }
                         />
 
                     </CardActionArea>
@@ -43,7 +45,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
               
           </Card>
 
-          <Box sx={{ mt: 1 }} className='fadeIn'>
+          <Box sx={{ mt: 1, display: isImageLoaded ? 'block':'none'}} className='fadeIn'>
               <Typography fontWeight={700}>{ product.title }</Typography>
               <Typography fontWeight={500}>{ `$${product.price}` }</Typography>
           </Box>
